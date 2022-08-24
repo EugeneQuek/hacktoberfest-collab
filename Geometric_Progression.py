5e7cf8110517992bab05fdbb0859113983e19b05
EugeneQuek <97581218+EugeneQuek@users.noreply.github.com>
2022-08-24 10:25:09 +0800

    Create Geometric_Progression.py

---
+++ Geometric_Progression.py
@@ -0,0 +1,10 @@
+# Write a function that determines if a list of integers follows a geometric progression
+# Test your function with the following lists:
+# list1 = [2, 4, 6, 8, 10]
+# list2 = [3, 6, 12, 24, 48]
+
+
+
+# *Bonus* Write a function that makes use of the previous function and returns the sum to infinity of a geometric progression if it exists
+# Test your function with the following lists:
+# list3 = [625, 125, 25, 5, 1]
